342e4acd964a34a26dd0bd6dd653e90f29c8abae
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2018-04-04 00:07:20 +0300

    POM.xml construction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         CmdLineService cmdLineService = new CmdLineService(new ClientServiceImpl(), new ProductServiceImpl(), new MenuServiceImpl());
         cmdLineService.goMainMenu();
     }
